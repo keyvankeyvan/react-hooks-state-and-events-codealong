91594a83072c73c0e2770b3022eba0d9049acb5d
Keyvan Alonso <keyvanalonso@gmail.com>
2022-06-28 23:32:54 -0400

    commit tree e4551136bb50253783c7e04036cb7d21775b1288

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false)
+
+  function handleClick() {
+    setIsOn(!isOn)
+  }
+
+  return (
+    <button onClick={handleClick}>{isOn ? "On" : "Off"}</button>
+  );
 }
 
 export default Toggle;
